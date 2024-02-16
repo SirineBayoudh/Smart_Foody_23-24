9390fd4ac1fbc059ece9a3792e5a187ed3c6f260
Sirine Bayoudh <sirine.bayoudh@esprit.tn>
2024-02-16 23:12:48 +0100

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
+
+    opens com.example.demo to javafx.fxml;
+    exports com.example.demo;
+    exports com.example.demo.Controllers;
+    opens com.example.demo.Controllers to javafx.fxml;
+    exports com.example.demo.Tools;
+    opens com.example.demo.Tools to javafx.fxml;
+}
