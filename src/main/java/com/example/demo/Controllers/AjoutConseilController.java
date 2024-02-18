5fec6426f9d9c64be443b8a1245e21c3799d188f
Yassine-Trabelsi10 <yassine.trabelsi@esprit.tn>
2024-02-18 13:15:14 +0100

    Affichage des conseils (coté client)

--- src/main/java/com/example/demo/Controllers/AjoutConseilController.java
+++ src/main/java/com/example/demo/Controllers/AjoutConseilController.java
@@ -1,20 +1,26 @@
 package com.example.demo.Controllers;
 
+import com.example.demo.Models.Conseil;
 import com.example.demo.Tools.MyConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.TextArea;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.IOException;
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.Properties;
 import java.util.ResourceBundle;
@@ -23,9 +29,22 @@ public class AjoutConseilController implements Initializable {
 
     @FXML
     private TextArea demandeLabel;
+    @FXML
+    private TableColumn<Conseil, java.util.Date> coldate;
+    @FXML
+    private TableColumn<Conseil, String> coldemande;
+    @FXML
+    private TableColumn<Conseil, Integer> colnote;
+    @FXML
+    private TableColumn<Conseil, String> colreponse;
+    @FXML
+    private TableColumn<Conseil, String> colstatut;
+    @FXML
+    private TableView<Conseil> table;
 
     Connection con = null;
     PreparedStatement st = null;
+    ResultSet rs = null;
 
     @FXML
     void ajoutConseil(ActionEvent event) {
@@ -39,7 +58,7 @@ public class AjoutConseilController implements Initializable {
             String insert = "INSERT INTO conseil(id_conseil, id_client, matricule, statut, demande, reponse, date_conseil) VALUES (?, ?, ?, ?, ?, ?, ?)";
             con = MyConnection.instance.getCnx();
             st = con.prepareStatement(insert);
-            st.setString(1, String.valueOf(9970023));
+            st.setString(1, String.valueOf(46));
             st.setString(2, String.valueOf(123123123));
             st.setInt(3, 321312312);
             st.setString(4, "en attente");
@@ -50,6 +69,7 @@ public class AjoutConseilController implements Initializable {
             st.setObject(7, date);
 
             int rowsAffected = st.executeUpdate();
+            showConseils();
             if (rowsAffected > 0) {
                 showAlert(Alert.AlertType.INFORMATION, "Success", "Conseil added successfully.");
                 sendEmail("yassiine.studies@gmail.com", "Smart Foody : Nouvelle demande reçue", demandeLabel.getText(), currentDate.toString());
@@ -117,6 +137,60 @@ public class AjoutConseilController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         MyConnection.getInstance();
-        //noteChoiceBox.getSelectionModel().selectFirst();
+        showConseils();
+    }
+
+    public ObservableList<Conseil> getConseils(){
+        ObservableList<Conseil> conseils = FXCollections.observableArrayList();
+        String query="select * from conseil WHERE id_client='123123123'";
+        con = MyConnection.instance.getCnx();
+        try {
+            st = con.prepareStatement(query);
+            rs = st.executeQuery();
+            while(rs.next()){
+                Conseil st = new Conseil();
+                st.setStatut(rs.getString("statut"));
+                st.setDemande(rs.getString("demande"));
+                st.setReponse(rs.getString("reponse"));
+                st.setNote(rs.getInt("note"));
+                st.setDate_conseil(rs.getDate("date_conseil"));
+                conseils.add(st)
+                ;            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return conseils;
+    }
+
+    public void showConseils(){
+        ObservableList<Conseil> list = getConseils();
+        table.setItems(list);
+        colstatut.setCellValueFactory(new PropertyValueFactory<Conseil,String>("statut"));
+        coldemande.setCellValueFactory(new PropertyValueFactory<Conseil,String>("demande"));
+        colreponse.setCellValueFactory(new PropertyValueFactory<Conseil,String>("reponse"));
+        colnote.setCellValueFactory(new PropertyValueFactory<Conseil,Integer>("note"));
+        coldate.setCellValueFactory(new PropertyValueFactory<Conseil, java.util.Date>("date_conseil"));
+    }
+
+    @FXML
+    void getData(MouseEvent event) {
+        if (event.getClickCount() == 2) { // Check for double-click
+            Conseil selectedConseil = table.getSelectionModel().getSelectedItem();
+            if (selectedConseil != null) {
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/demo/DetailConseilWindow.fxml"));
+                    Parent root = loader.load();
+
+                    DetailWindowController controller = loader.getController();
+                    controller.initData(selectedConseil);
+
+                    Stage stage = new Stage();
+                    stage.setScene(new Scene(root));
+                    stage.show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
