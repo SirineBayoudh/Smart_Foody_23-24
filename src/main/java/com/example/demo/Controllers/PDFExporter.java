ea0a56279486328e77747182482ca44c7609e34f
zeineb-m <zeineb.maatalli@esprit.tn>
2024-02-21 11:15:01 +0100

    Update PDF exportAll

--- src/main/java/com/example/demo/Controllers/PDFExporter.java
+++ src/main/java/com/example/demo/Controllers/PDFExporter.java
@@ -4,7 +4,9 @@ import com.example.demo.Models.Stock;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+import javafx.collections.ObservableList;
 
 import java.awt.Desktop;
 import java.io.File;
@@ -39,6 +41,48 @@ public class PDFExporter {
         }
     }
 
+    public static boolean exportAllStocksToPDF(String fileName, ObservableList<Stock> allStocks) {
+        try {
+            Document document = new Document();
+            PdfWriter.getInstance(document, new FileOutputStream(fileName));
+            document.open();
+
+            document.add(new Paragraph("All Stocks Report"));
+
+            // Créer un tableau avec 4 colonnes (Référence Produit, Marque, Quantité, NbVendu)
+            PdfPTable table = new PdfPTable(4);
+
+            // Ajouter les en-têtes de colonne au tableau
+            table.addCell("Référence Produit");
+            table.addCell("Marque");
+            table.addCell("Quantité");
+            table.addCell("NbVendu");
+
+            for (Stock stock : allStocks) {
+                // Ajouter les données de chaque stock au tableau
+                table.addCell(String.valueOf(stock.getProduitRef()));
+                table.addCell(stock.getProduitMarque());
+                table.addCell(String.valueOf(stock.getQuantite()));
+                table.addCell(String.valueOf(stock.getNbVendu()));
+            }
+
+            // Ajouter le tableau au document PDF
+            document.add(table);
+
+            document.close();
+
+            System.out.println("Export PDF réussi.");
+
+            // Ouvrir le fichier PDF avec l'application par défaut
+            openPDFFile(fileName);
+
+            return true; // Indicateur de succès
+        } catch (DocumentException | FileNotFoundException e) {
+            e.printStackTrace();
+            System.err.println("Erreur lors de l'export PDF.");
+            return false; // Indicateur d'échec
+        }
+    }
 
     private static void openPDFFile(String fileName) {
         try {
