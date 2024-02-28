e64c2fa2a325ca267d8719730742f2ca1cd69433
Sirine Bayoudh <sirine.bayoudh@esprit.tn>
2024-02-28 17:35:36 +0100

    redirection_navebarre

---
+++ src/main/java/com/example/demo/Controllers/NavbarreController.java
@@ -0,0 +1,105 @@
+package com.example.demo.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NavbarreController implements Initializable {
+
+    @FXML
+    private BorderPane centerPane;
+    @FXML
+    private Button BtnConseil;
+    @FXML
+    private Button btnAccueil;
+    @FXML
+    private Button btnProduitRecommande;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // Initialisation initiale, si nécessaire
+    }
+
+    @FXML
+    private void handleClick(MouseEvent event) {
+       /* Object source = event.getSource();
+        if (source instanceof Label) {
+            Label label = (Label) source;
+            switch (label.getId()) {
+                case "labelAccueil":
+                    // Chargez la vue d'accueil, si vous en avez une
+                    break;
+                case "labelProduits":
+                    loadPage("/com/example/pitest/fxml/panier.fxml");
+                    break;
+                // Ajoutez d'autres cas pour les autres labels
+            }
+        } else if (source instanceof ImageView) {
+            ImageView imageView = (ImageView) source;
+            // Gérez les clics sur les icônes ici, similaire aux labels
+        }*/
+    }
+
+    @FXML
+    private void loadProduit() {
+    }
+
+    @FXML
+    private void loadPanier() {
+        //clear();
+        //loadPage("/com/example/demo/panier.fxml");
+    }
+
+    @FXML
+    void loadConseil() {
+        //BtnConseil.setStyle("-fx-background-color: #56AB2F");
+        //System.out.println("changed");
+        //loadPage("/com/example/demo/ajoutConseil.fxml");
+    }
+
+    public void clear(){
+        BtnConseil.setStyle("-fx-background-color: #ffffff");
+    }
+
+    @FXML
+    void loadAccueil(ActionEvent event) {
+        btnProduitRecommande.setStyle("-fx-background-color: #ffffff");
+        btnAccueil.setStyle("-fx-background-color: #56ab2f");
+        loadPage("/com/example/demo/PageAccueil.fxml");
+
+    }
+
+    @FXML
+    void loadProduitRecommande(ActionEvent event) {
+        btnAccueil.setStyle("-fx-background-color: #ffffff");
+        btnProduitRecommande.setStyle("-fx-background-color: #56ab2f");
+        loadPage("/com/example/demo/recette.fxml");
+    }
+
+
+    private void loadPage(String page) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(page));
+            if (centerPane != null) {
+                centerPane.setCenter(root);
+            } else {
+                System.out.println("centerPane is null");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/example/demo/Controllers/dashboardController.java
+++ src/main/java/com/example/demo/Controllers/dashboardController.java
@@ -66,10 +66,23 @@ public class dashboardController implements Initializable {
     }
     @FXML
     private void dashboard() {
-        centerPane.setCenter(innerPane);
-        btn_home.getStyleClass().add("btn_home");
+            try {
+                Parent navbarreRoot = FXMLLoader.load(getClass().getResource("/com/example/demo/navbarre.fxml"));
 
-    }
+                Stage navbarreStage = new Stage();
+                navbarreStage.setScene(new Scene(navbarreRoot));
+                navbarreStage.setHeight(700);
+                navbarreStage.setWidth(1200);
+                navbarreStage.setTitle("Smart foody");
+                navbarreStage.show();
+
+                Stage dashboardStage = (Stage) btn_home.getScene().getWindow();
+                dashboardStage.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
 
     @FXML
     private void stock() {
@@ -101,6 +114,8 @@ public class dashboardController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
+
 
 
 }
